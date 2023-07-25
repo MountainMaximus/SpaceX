8b65fe57dd49e1cf33773e772ddccbdfff221355
Max <ryazanov-maximum@mail.ru>
2023-07-26 00:29:58 +0700

    the project has been created

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const production = process.env.MODE_ENV === "production";
+
+module.exports = {
+  entry: { myAppName: path.resolve(__dirname, "./src/index.tsx") },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: production ? "bundle.[contenthash].js" : "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: "ts-loader",
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        exclude: /node_modules/,
+        use: [
+          production ? MiniCssExtractPlugin.loader : "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              sourceMap: !production,
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: !production,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".tsx", ".scss"],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "SpaceX",
+      template: "./src/index.html",
+      favicon: "./public/favicon.ico",
+    }),
+    new MiniCssExtractPlugin({
+      filename: production ? "[name].[contenthash].css" : "[name].css",
+    }),
+  ],
+  devServer: {
+    port: 3001,
+    hot: true,
+  },
+  mode: production ? "production" : "development",
+};
